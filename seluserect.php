abe5bebb8b43290ee085d262232675103b5e8800
AxSa <sam@mr-smart.ru>
2013-07-15 13:07:02 +0600

    release version 1.0

---
+++ seluserect.php
@@ -0,0 +1,77 @@
+<?php
+/*
+Plugin Name: Seluserect
+Plugin URI: http://test.wp.mr-smart.ru/
+Description: поле выбора пользователя
+Version: 1.0
+Author: kisagiSensei
+Author URI:http://test.wp.mr-smart.ru/
+Description: поле выбора пользователя
+*/
+/*  Copyright 2008  Jenyay  (email : jenyay.ilin@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+add_action( 'show_user_profile', 'extra_user_profile_fields' );
+add_action( 'edit_user_profile', 'extra_user_profile_fields' );
+ 
+function extra_user_profile_fields( $user ) { ?>
+<h3><?php _e("Extra profile information", "blank"); ?></h3>
+ 
+<table class="form-table">
+<tr>
+<th><label for="address"><?php _e("Address"); ?></label></th>
+<td>
+<input type="text" name="address" id="address" value="<?php echo esc_attr( get_the_author_meta( 'address', $user->ID ) ); ?>" class="regular-text" /><br />
+<span class="description"><?php _e("Please enter your address."); ?></span>
+</td>
+</tr>
+<tr>
+<th><label for="city"><?php _e("City"); ?></label></th>
+<td>
+<input type="text" name="city" id="city" value="<?php echo esc_attr( get_the_author_meta( 'city', $user->ID ) ); ?>" class="regular-text" /><br />
+<span class="description"><?php _e("Please enter your city."); ?></span>
+</td>
+</tr>
+<tr>
+<th><label for="province"><?php _e("Province"); ?></label></th>
+<td>
+<input type="text" name="province" id="province" value="<?php echo esc_attr( get_the_author_meta( 'province', $user->ID ) ); ?>" class="regular-text" /><br />
+<span class="description"><?php _e("Please enter your province."); ?></span>
+</td>
+</tr>
+<tr>
+<th><label for="postalcode"><?php _e("Postal Code"); ?></label></th>
+<td>
+<input type="text" name="postalcode" id="postalcode" value="<?php echo esc_attr( get_the_author_meta( 'postalcode', $user->ID ) ); ?>" class="regular-text" /><br />
+<span class="description"><?php _e("Please enter your postal code."); ?></span>
+</td>
+</tr>
+</table>
+<?php }
+ 
+add_action( 'personal_options_update', 'save_extra_user_profile_fields' );
+add_action( 'edit_user_profile_update', 'save_extra_user_profile_fields' );
+ 
+function save_extra_user_profile_fields( $user_id ) {
+ 
+if ( !current_user_can( 'edit_user', $user_id ) ) { return false; }
+ 
+update_user_meta( $user_id, 'address', $_POST['address'] );
+update_user_meta( $user_id, 'city', $_POST['city'] );
+update_user_meta( $user_id, 'province', $_POST['province'] );
+update_user_meta( $user_id, 'postalcode', $_POST['postalcode'] );
+}
+?>
